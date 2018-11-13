39af96f2a9b60caa96d9e0f21ae8c761821ae3cd
Quan Bui <quanb1997@gmail.com>
2018-11-13 16:41:21 -0500

    Add files via upload

--- tweet_downloader/tweet_downloader.py
+++ tweet_downloader/tweet_downloader.py
@@ -8,6 +8,7 @@ import time
 import re
 
 
+
 emoji_pattern = re.compile("["
      #   u"\U0001F600-\U0001F64F"  # emoticons
      #   u"\U0001F300-\U0001F5FF"  # symbols & pictographs
@@ -42,10 +43,22 @@ except:
 if len(screen_name) == 0:
 	screen_name = twitter["screen_name"]
 	
-print("> downloading tweets from ", screen_name)
+print("> Downloading tweets from ", screen_name)
 	
 # list of user tweets
 user_tweets = []
+
+#Progress bar
+def progressBar(value, endvalue, bar_length=20):
+    percent = float(value) / endvalue
+    arrow = u"\u2588" * int(round(percent * bar_length) - 1)
+    spaces = ' ' * (bar_length)
+
+    sys.stdout.write("\rPercent: {0} {1}%".format(arrow, int(round(percent * 100)), spaces))
+    sys.stdout.flush()
+
+progressBar(0,20,20)
+
 
 # request for tweets, save them, save the id of the oldest one (max_id)
 try:
@@ -55,11 +68,16 @@ try:
 									  exclude_replies=False,
 									  tweet_mode = "extended")
 except Exception:
-	print("> ERROR: Not a valid screen name!")
+	print("\n> ERROR: Not a valid screen name!")
 	quit()
 user_tweets.extend(recent_tweets)
 max_id = user_tweets[-1].id - 1
 
+
+
+
+#print("> Downloading Tweets", end="", flush=True)
+count = 0
 while len(recent_tweets) > 0:
     recent_tweets = api.user_timeline(screen_name=screen_name,
                                       count=200,
@@ -72,13 +90,22 @@ while len(recent_tweets) > 0:
     user_tweets.extend(recent_tweets)
     max_id = user_tweets[-1].id - 1
 
-    print("> {0:4} tweets saved!".format(len(user_tweets)))
-
-print("> Finished with ", len(user_tweets), " tweets saved!")
+    #print("> {0:4} tweets saved!".format(len(user_tweets)))
+    #print( 	u"\u2588\u2588", end = "", flush=True)
+    progressBar(count, 20, 20)
+    count+=1
+progressBar(20,20,20)
+#if count < 20:
+#    for i in range(count, 20):
+#        print( u"\u2588\u2588", end = "", flush=True)
+print("\n> Finished with ", len(user_tweets), " tweets saved!")
 
 # Grabs the tweet objects we want.
+
+political_keywords = ["guns","trump","2020","dems","democrats","republicans","vote","politics","maga"]
+
 def format_tweet(tweet):
-    return {
+	return {
         "tweet_id" : tweet.id,
         "username": tweet.user.name,
         "mentions" : tweet.entities["user_mentions"],
@@ -90,8 +117,10 @@ def format_tweet(tweet):
         "retweets": tweet.retweet_count,
         "source" : tweet.source
     }
-formatted_tweets = list(map(format_tweet, user_tweets))
+trim = len(user_tweets)
 
+formatted_tweets = list(map(format_tweet, user_tweets[:trim]))
+formatted_tweets_trimmed = list(map(format_tweet, user_tweets[:1500]))
 try:
     # Creates tweets folder
     os.mkdir("tweets")
@@ -138,10 +167,15 @@ def pull_status_obj(tweet):
     return {
         "json" : tweet._json
     }
-status_json = list(map(pull_status_obj, user_tweets))
+
+
+status_json = list(map(pull_status_obj, user_tweets[:trim]))
+status_json_trimmed = list(map(pull_status_obj, user_tweets[:1500]))
 
 file_name = 'full_tweets.json'
 completeName = os.path.join(save_path, file_name)
 with open(completeName, "w") as output:
     json.dump(status_json, output)
 print("> Created ", file_name, " in ", save_path)
+
+print("> {} tweets were stored in each of the files.".format(trim))
